654bbe387713fc65a0ca268de1ef71f9497996b2
Narahari <narahari517@gmail.com>
2024-08-27 15:21:24 +0530

    special-variables

--- 05-special-variables.sh
+++ 05-special-variables.sh
@@ -2,3 +2,9 @@
 
 echo "All variables passed to the script: $@"
 echo "Number of variables passed: $#"
+echo "Name of the running script: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of the current user: $HOME"
+echo "PID of the script executing now: $$"
+sleep 100 &
+echo "PID of the last background command: $!"

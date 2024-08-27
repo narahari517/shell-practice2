721819099b024e19b9e696e9d0d9b17729077d2f
Narahari <narahari517@gmail.com>
2024-08-27 15:04:31 +0530

    array

--- 04-array.sh
+++ 04-array.sh
@@ -3,3 +3,7 @@
 Players=("Sachin" "Dravid" "Ganguly")
 
 echo "First player is: ${Players[0]}"
+echo "Second player is: ${Players[1]}"
+echo "Third player is: ${Players[2]}"
+
+echo "All players: ${Players[@]}"

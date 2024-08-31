316b74caeb642ad87f1d8499b009f41d42861142
Narahari <narahari517@gmail.com>
2024-08-31 11:49:34 +0530

    backup

--- 11-backup.sh
+++ 11-backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 source_dir=$1
-destination_dir=$2
-days=${3:-14} #if user doesn't give days, take it as 14
-timestamp=$(date +%y-%m-%d-%H-%M-%S)
+desination_dir=$2
+days=${3:-14} #if not provided by user, take as 14
+timestamp=$(date +%y-%m-%d-%H-%D-%S)
 
 R="\e[31m"
 G="\e[32m"
@@ -11,8 +11,7 @@ Y="\e[33m"
 N="\e[0m"
 
 usage(){
-    echo -e "$R USAGE::$N sh 11-backup.sh <source> <destination> <days(optional)>"
-    exit 1
+    echo -e "$R USAGE::$N sh 11-backup.sh <source> <destination> <days(optional)>
 }
 
 if [ $# -lt 2 ]
@@ -23,7 +22,6 @@ fi
 if [ ! -d $source_dir ]
 then
     echo -e "$R $source_dir does not exist, please check $N"
-    exit 1
 fi
 
 if [ ! -d $destination_dir ]
@@ -33,30 +31,28 @@ fi
 
 FILES=$(find $source_dir -name "*.log" -mtime +$days)
 
-if [ ! -z $FILES ] #true if there are no files, ! makes the expression false
+if [ ! -z $FILES ]
 then
     echo "files are found"
     zip_file="$destination_dir/app-logs-$timestamp.zip"
     find ${source_dir} -name "*.log" -mtime +$days | zip $zip_file -@
-
-    #check if the zip file is successfully created
+    
+    #check if the files are zipped
     if [ -f $zip_file ]
     then
-        echo "zipped the log files successfully older than $days"
-        #remove the files after zipping
+        echo -e "$G zipped log files older than $days days successfully $N"
+
+        #remove log files post zip
         while IFS= read -r file
         do
             echo "Deleting file: $file"
             rm -rf $file
         done <<< $FILES
-
     else
-        echo "zipping files failed"
+        echo -e "$R zipping log files failed $N"
         exit 1
     fi
-
 else
-    echo "no files older than $days"
+    echo "files older than $days days not found"
+    exit 1
 fi
-
-
